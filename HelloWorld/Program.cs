e5dc122ef916a718f7205ded52a2481c1ccf1f85
KorshunovCareeria <158146367+KorshunovCareeria@users.noreply.github.com>
2024-10-07 10:03:02 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace HelloWorld
@@ -7,13 +7,32 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-           
+            // Kysytään käyttäjältä lukua
+            Console.Write("Anna luku: ");
+
+            // Tehdään kertoluku muuttuja ja asetetaan sille arvo jonka käyttäjä on antanut
+            int kertoluku = int.Parse(Console.ReadLine());
+
+            // Tulostetaan kertotaulu sille luvulle
+            Console.WriteLine("Kertotaulu luvulle " + kertoluku + ":");
+
+            // for-silmukka
+            for (int i = 1; i <= 10; i++)
+            {
+                // Käytetään kertolaskukaava metodia että saadaan tulos
+                // Esim: 3 * 4 = 12
+                // Esim: i / luku1 = i / kertoluku = vastaus
+                Console.WriteLine(Kertolaskukaavalla(i, kertoluku));
+            }
         }
 
+        // Tämä metodi tuo vastauksen:
+        // luku1 * luku2 == (Luku1 * Luku2)
+        // eli 3 * 4 == (3*4)
         public static string Kertolaskukaavalla(int luku1, int luku2)
         {
-           
-            return "";
+            // Palautetaan luku1 * luku2 lasku
+            return luku1 + " * " + luku2 + " = " + (luku1 * luku2);
         }
     }
 }
